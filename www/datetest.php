52a2a6721e9085112e8bd835c9b04b2ac9cceab8
Maciej Sawka <maciejsawka@gmail.com>
2022-05-06 16:14:43 +0200

    Delete datetest.php
    Just a random file that was left over from some tests

--- www/datetest.php
+++
@@ -1,5 +0,0 @@
-<?php
-    include_once($_SERVER['DOCUMENT_ROOT'] . '/models/image.php');
-
-    echo Image::insert(1, 'muj pliczek', 'bardzo fajny pliczek ktory zrobilem', 'png', 'AAAAAAAAAAAAAAAAAAAA');
-?>
